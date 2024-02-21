b17097277536f992bf7170bba9a82f67b92bb390
Huseyn Hajiyev <hhajiyev@hotmail.com>
2024-02-21 14:00:30 +0400

    added eye candy to loading splash screen

--- src/components/SplashScreens/LoadingSplash.tsx
+++ src/components/SplashScreens/LoadingSplash.tsx
@@ -1,7 +1,19 @@
+import { Box, CircularProgress } from "@mui/material"
 
 const LoadingSpalsh = () => {
   return (
-    <div>LoadingSpalsh</div>
+    <Box minHeight={'100dvh'} minWidth={'100dvw'} position={'relative'}>
+       <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)'
+        }}
+       >
+        <CircularProgress size={100} />
+       </Box>
+    </Box>
   )
 }
 
